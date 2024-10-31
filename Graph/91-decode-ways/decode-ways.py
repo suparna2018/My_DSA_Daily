26db07fc852a02891106fbbb95caaa0117b398fa
SuparnaB <40120628+suparna2018@users.noreply.github.com>
2024-10-31 15:55:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (13.37%) - LeetSync

--- Graph/91-decode-ways/decode-ways.py
+++ Graph/91-decode-ways/decode-ways.py
@@ -24,4 +24,24 @@ class Solution:
             return res
 
         n = len(s)
-        return dp(0)
+        memo={n:1}
+        for ind in range(n-1,-1,-1):
+            if s[ind] == "0":  # Invalid if the current segment starts with '0'
+                memo[ind]=0
+            else:
+                if ind in memo:  # Return memoized result if available
+                    return memo[ind]
+
+                # Take 1 character
+                res =memo[ind+1] 
+                
+                # Take 2 characters
+                if ind < n - 1 and (
+                    (s[ind] == "1" and s[ind + 1] in "0123456789") or
+                    (s[ind] == "2" and s[ind + 1] in "0123456")
+                ):
+                    res +=memo[ind+2] 
+
+                memo[ind] = res
+            
+        return memo[0]
