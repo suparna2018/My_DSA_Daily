fb4669484f79fd365667e171e8a620eeea9a79db
SuparnaB <40120628+suparna2018@users.noreply.github.com>
2024-10-31 15:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (13.37%) - LeetSync

---
+++ Graph/91-decode-ways/decode-ways.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+
+        def dp(ind):
+            if ind == n:  # Base case: reached end of string
+                return 1
+            if s[ind] == "0":  # Invalid if the current segment starts with '0'
+                return 0
+            if ind in memo:  # Return memoized result if available
+                return memo[ind]
+
+            # Take 1 character
+            res = dp(ind + 1)
+            
+            # Take 2 characters
+            if ind < n - 1 and (
+                (s[ind] == "1" and s[ind + 1] in "0123456789") or
+                (s[ind] == "2" and s[ind + 1] in "0123456")
+            ):
+                res += dp(ind + 2)
+
+            memo[ind] = res  # Memoize result
+            return res
+
+        n = len(s)
+        return dp(0)
