6210828796ecbbf6813da3566e74f37cd9511c46
SuparnaB <40120628+suparna2018@users.noreply.github.com>
2024-11-13 12:37:56 +0530

    Time: 5 ms (9.39%) | Memory: 16.8 MB (17.06%) - LeetSync

--- Graph/67-add-binary/add-binary.py
+++ Graph/67-add-binary/add-binary.py
@@ -5,21 +5,16 @@ class Solution:
         b = b.zfill(maxlen)
         
         res = []
-        c = 0  # Initialize carry to 0
-
-        # Iterate from the end to the beginning
+        c = 0  
         for i in range(maxlen - 1, -1, -1):
-            bit_a = int(a[i])  # Convert character to integer
-            bit_b = int(b[i])  # Convert character to integer
-
-            # Total sum of both bits and the carry
+            bit_a = int(a[i])  
+            bit_b = int(b[i])  
+     
             tot = bit_a + bit_b + c
-            res.append(str(tot % 2))  # Append the result bit
-            c = tot // 2  # Update carry
-
-        # If there's a carry left after the loop, append it
+            res.append(str(tot % 2))  
+            c = tot // 2  
+        
         if c:
             res.append(str(c))
 
-        # The result is currently reversed, so reverse it back
         return ''.join(res[::-1])
