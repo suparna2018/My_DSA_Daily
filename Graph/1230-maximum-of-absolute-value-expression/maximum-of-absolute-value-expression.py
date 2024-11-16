6e84f64263d6dbf8ea18dbdc03d4a2930a7b793f
SuparnaB <40120628+suparna2018@users.noreply.github.com>
2024-11-16 19:54:26 +0530

    Time: 159 ms (6.56%) | Memory: 23.6 MB (82.56%) - LeetSync

--- Graph/1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.py
+++ Graph/1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.py
@@ -1,20 +1,16 @@
 class Solution:
     def maxAbsValExpr(self, arr1: List[int], arr2: List[int]) -> int:
-        n = len(arr1)
-        max_result = float('-inf')
-        
-        # Iterate over all 8 combinations of signs
-        for p1 in [1, -1]:
-            for p2 in [1, -1]:
-                for p3 in [1, -1]:
-                    max_val = float('-inf')
-                    min_val = float('inf')
-                    
+        ans=-1e9
+        n=len(arr1)
+        for p1 in [-1,1]:
+            for p2 in [-1,1]:
+                for p3 in [-1,1]:
+                    maxVal=-1e9
+                    minVal=1e9
                     for i in range(n):
-                        value = p1 * arr1[i] + p2 * arr2[i] + p3 * i
-                        max_val = max(max_val, value)
-                        min_val = min(min_val, value)
-                    
-                    max_result = max(max_result, max_val - min_val)
-        
-        return max_result
+                        val=p1*arr1[i]+p2*arr2[i]+p3*i
+                        maxVal=max(maxVal,val)
+                        minVal=min(minVal,val)
+
+                    ans=max(ans,maxVal-minVal)
+        return ans
